0abc53fa6be30a6f7e6dc388b63c48ab08819105
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-06-16 16:33:20 -0300

    create pack aula 02 and 03

---
+++ projetos/src/aula02/Teste.java
@@ -0,0 +1,21 @@
+package aula02;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Caneta c1 = new Caneta();
+		c1.modelo = "Bic";
+		c1.cor = "Preto";
+		c1.ponta = 0.5;
+		c1.carga = 100;
+		c1.tampada = false;
+		
+		c1.destampar();
+		
+		
+		c1.status();
+		c1.rabiscar();
+		
+	}
+}
---
+++ projetos/src/aula03/Caneta.java
@@ -0,0 +1,33 @@
+package aula03;
+
+public class Caneta {
+	public String modelo;
+	public String cor;
+	private Double ponta;
+	protected int carga;
+	protected boolean tampada;
+	
+	public void status() {
+		System.out.println("Modelo " + this.modelo);
+		System.out.println("Uma caneta " + this.cor);
+		System.out.println("A ponta: " + this.ponta);
+		System.out.println("Carga: " + this.carga);
+		System.out.println("Está tampada: " + this.tampada);
+	}
+	
+	protected void rabiscar() {
+		if (this.tampada == true) {
+			System.out.println("Erro! não posso rabiscar");
+		} else {
+			System.out.println("Já estou rabiscando ");
+		}
+	}
+	
+	protected void tampar() {
+		this.tampada = true;
+	}
+	
+	void destampar() {
+		this.tampada = false;
+	}
+}
