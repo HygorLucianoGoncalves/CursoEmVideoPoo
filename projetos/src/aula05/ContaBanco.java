3d7512ef5437ff060e8e3e5c46ea1ce544ec4585
Hygor Luciano <128493484+HygorLucianoGoncalves@users.noreply.github.com>
2023-06-18 13:51:18 -0300

    Update ContaBanco.java

--- projetos/src/aula05/ContaBanco.java
+++ projetos/src/aula05/ContaBanco.java
@@ -58,30 +58,73 @@ public class ContaBanco {
 		this.status = status;
 	}
 
-	private void getStatus() {
+	public boolean getStatus() {
 		return this.status;
-
 	}
 
 	// Métodos
-	public void abrirConta() {
+	public void abrirConta(String tipo) {
+		setTipo(tipo);
+		setStatus(true);
+		if (tipo == "CC") {
+			setSaldo(50);
+		} else if (tipo == "CP") {
+			setSaldo(150);
+		}
 
 	}
 
 	public void fecharConta() {
 
+		if (saldo > 0) {
+			System.out.println("Conta tem dinheiro");
+		} else if (saldo < 0) {
+			System.out.println("Conta com débito");
+		} else {
+			setStatus(false);
+		}
 	}
 
-	public void deposita() {
+	public void deposita(double valor) {
 
+		if (getStatus() == true) {
+			setSaldo(getSaldo() + valor);
+		} else {
+			System.out.println("Impossivel Depositar");
+		}
 	}
 
-	public void sacar() {
+	public void sacar(double valor) {
+		if (getStatus() == true) {
+			if (getSaldo() >= valor) {
+				setSaldo(getSaldo() - valor);
+			} else {
+				System.out.println("Saldo Insuficiente ");
+			}
 
+		} else {
+			System.out.println("Impossivel sacar, Conta está fechada ");
+		}
 	}
 
 	public void pagarMensal() {
-
+		double valorDaMensalidade = 0;
+		if (getTipo() == "CC") {			
+			valorDaMensalidade = 12;
+		} else if(getTipo() == "CP") { 
+			valorDaMensalidade = 20;
+		} else {
+			if (getStatus() == true) {
+				if (getSaldo() > valorDaMensalidade) {
+					setSaldo(getSaldo() - valorDaMensalidade); 
+				} else {
+					System.out.println("Saldo Insuficiente");
+				}
+			} else {
+				System.out.println("Impossivel Pagar");
+			}
+		}
+				
 	}
 
 }
